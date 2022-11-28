ed0171cc09f46844c18cb68332fa55a106417b28
user <user@DESKTOP-U59IHVT>
2022-11-28 14:32:56 +0900

    web.xml push

--- Project/src/main/java/com/domain/BoardDAO.java
+++ Project/src/main/java/com/domain/BoardDAO.java
@@ -8,5 +8,30 @@ public class BoardDAO {
 	SqlSessionFactory sqlSessionFactory = SqlSessionManager.getSqlSession();
 	SqlSession sqlSession = sqlSessionFactory.openSession();
 	
-	//
+	// 날짜 얻기
+	public String getDate() {
+		String date =null;
+		try {
+
+			date = sqlSession.selectOne("getDate");
+
+			if (date!=null) {
+
+				System.out.println("DAO : Get Date 성공");
+				sqlSession.commit();
+
+			} else {
+
+				sqlSession.rollback();
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			sqlSession.close();
+		}
+		return date;
+	}
+	
+	
 }
